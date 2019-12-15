326c63b0bc7756714a599645b9818e7ea8e883cd
m-santoro <m.santoro70@studenti.unisa.it>
2019-12-15 19:30:35 +0100

    Implemented iterative dfs

--- Ex3/DFS.py
+++ Ex3/DFS.py
@@ -1,2 +1,32 @@
 from TdP_collections.graphs.graph import Graph
 
+def iterative_dfs(graph, root):
+    dictionary = {}
+
+    dictionary[root] = None
+    root.set_visited(True)
+
+    node = root
+    while True:
+        if not node.is_visited():
+            dictionary[node] = graph.get_edge(node.get_visited_from(), node)
+            node.set_visited(True)
+
+        # cicle edges to find the first not visited neighbour vertex
+        next_node = None
+        for edge in graph.incident_edges(node):
+            opposite = edge.opposite(node)
+            if not opposite.is_visited():
+                next_node = opposite
+                break
+        
+        if next_node is None:    # No other edges to visit
+            if node == root:     # exit if in root
+                break
+            else:                # Go back to previous node
+                node = node.get_visited_from()
+        else:                    # Set next edge to visit
+            next_node.set_visited_from(node)
+            node = next_node
+
+    return dictionary
